dd2813def29c21ec7f43a5fce318a7560c547523
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2023-05-31 12:54:44 -0300

    Update dom.js

--- desafio2/dom.js
+++ desafio2/dom.js
@@ -1,1 +1,9 @@
+/* DESAFIO 2
+1. Encontrar el segundo párrafo en la página
+2. Agregue una clase de "resaltado", o
+3. Eliminar la clase de "resaltar" si ya está presente
+
+ function Highlight() {
+   // tu codigo aqui
+ }
 
